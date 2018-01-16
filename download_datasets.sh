49a0b7f4c953c635888081cd51b5978d25e26455
Marianne Linhares Monteiro <mariannelinharesm@gmail.com>
2018-01-16 10:38:26 -0300

    Adding download dataset

---
+++ download_datasets.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# downloads and unzips Religion dataset
+mkdir religion_dataset
+wget -qO- https://github.com/marcotcr/lime-experiments/blob/master/religion_dataset.tar.gz?raw=true | tar xvz -C religion_dataset
+
+# downloads and unzips Multi-polarity datasets
+mkdir processed_acl
+wget -qO- https://www.cs.jhu.edu/~mdredze/datasets/sentiment/processed_acl.tar.gz | tar xvz -C processed_acl
